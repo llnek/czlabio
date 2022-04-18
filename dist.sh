314b57c855cd299fb4a8eaa797d83eb9494dc694
Kenneth Leung <ccllnekk@gmail.com>
2022-04-18 15:40:24 -0700

    synced lastest vendor files

---
+++ dist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+cd ./p
+cwd=`pwd`
+echo $cwd
+for x in `ls`
+do
+  cd $x
+  #f=`basename "$x" ".mkv"`
+  echo "inside " `pwd`
+  gulp
+  rm ./dist/game.js
+  cd ..
+  #ffmpeg -i $f.mkv -vcodec copy -acodec copy $f.mp4
+  #rm $f.mkv
+done
